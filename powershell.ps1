e51f9ca8e9188c8e6deb9b416ddf8b64fcf61106
sayment <sametozabaci@outlook.com>
2017-10-13 07:59:42 +0300

    Add files via upload

---
+++ powershell.ps1
@@ -0,0 +1,10 @@
+﻿Get-WmiObject -Class Win32_Processor -ComputerName . | Select-Object -Property [a-z]*
+
+Get-WmiObject -Class Win32_BIOS -ComputerName .
+
+Get-WmiObject -Class Win32_LocalTime -ComputerName . | Select-Object -Property [a-z]*
+
+Get-WmiObject -Class Win32_Service -ComputerName . | Select-Object -Property Status,Name,DisplayName
+
+Get-Service | Where-Object {$_.Status -eq "Running"}
+
